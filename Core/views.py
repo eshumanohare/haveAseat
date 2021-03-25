d08a126385c05ff2ab4674d6897666e1bd247b79
Bhaskar Gupta <guptabhanu1999@gmail.com>
2021-03-25 17:50:11 +0530

    Added student panel

--- Core/views.py
+++ Core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def adminPanel(request):
     return render(request, "admin-panel.html", context = {})
+
+def studentPanel(request):
+    return render(request, "student-panel.html", context = {})
+
+def courseDescription(request):
+    return render(request, "course-description.html", context = {})
