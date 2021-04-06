d18c9781f2ff220da5a3bbae74a561f7303b3b2d
eshumanohare <37545945+eshumanohare@users.noreply.github.com>
2021-04-06 14:51:02 +0530

    Update urls.py

--- Core/urls.py
+++ Core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin-panel/", views.adminPanel, name = "adminPanel"),
     path("student-panel/", views.studentPanel, name = "studentPanel"),
     path("course-description/", views.courseDescription, name = "courseDescription"),
+    path("", views.homepage, name="homepage"),
 ]
