001246dc9f5c9f6c4a0a32988fa5052877460fc0
Vaibhav <vk10decvaibhav@gmail.com>
2021-04-14 13:17:05 +0530

    Fix

--- Core/models.py
+++ Core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db import connection
 
 class Branch(models.Model):
     branchName = models.CharField(max_length = 100)
@@ -9,12 +8,26 @@ class Branch(models.Model):
         verbose_name = "Branch"
         verbose_name_plural = "Branches"
 
+    def __str__(self):
+        return self.branchName
+
+class UserRole(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    role = models.CharField(max_length = 50)
+
+    class Meta:
+        verbose_name = "UserRole"
+        verbose_name_plural = "UserRoles"
+
 class Department(models.Model):
     departmentName = models.CharField(max_length = 100)
 
     class Meta:
-        verbose_name = "Branch"
-        verbose_name_plural = "Branches"
+        verbose_name = "Department"
+        verbose_name_plural = "Departments"
+
+    def __str__(self):
+        return self.departmentName
 
 class Admin(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -47,29 +60,54 @@ class DOAA(models.Model):
         verbose_name_plural = "DOAA"
 
 class Faculty(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     profilePicture = models.ImageField(upload_to = "Profile Pictures/Faculties/")
-    profielLink = models.URLField()
+    profileLink = models.URLField()
 
     class Meta:
         verbose_name = "Faculty"
         verbose_name_plural = "Faculties"
 
 class Course(models.Model):
-    code = models.CharField(max_length=7, null=True,default="")
-    section = models.CharField(max_length = 3)
+    section = models.CharField(max_length = 1)
     courseName = models.CharField(max_length = 50)
     credits = models.IntegerField(default = 0)
     studentCap = models.IntegerField(default = 0)
     courseDescription = models.TextField()
     program = models.CharField(max_length = 50)
+    department = models.ForeignKey(Department, on_delete = models.SET_NULL, null = True)
 
-class has_pre_req(models.Model):
-    from_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="from_course")
-    to_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="to_course")
+class CourseFaculty(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete = models.CASCADE, related_name = "teaches")
+    course = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = "taughtBy")    
 
-class has_anti_req(models.Model):
-    from_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="anti_from_course")
-    to_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="anti_to_course")
+    class Meta:
+        verbose_name = "CourseFaculty"
+        verbose_name_plural = "CourseFaculties"
 
 class Announcement(models.Model):
     user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    timeStamp = models.DateTimeField(auto_now = True)
+    content = models.CharField(max_length = 500)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    timeStamp = models.DateTimeField(auto_now = True)
+    content = models.CharField(max_length = 500)
+    parentComment = models.ForeignKey("Comment", related_name = "parentOf", on_delete = models.SET_NULL, null = True)
+
+class PreReq(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = "preReqs")
+    preReqCourse = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = "preReqOf")
+
+    class Meta:
+        verbose_name = "PreReq"
+        verbose_name_plural = "PreReqs"
+
+class AntiReq(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = "antiReqs")
+    antiReqCourse = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = "antiReqOf")
+
+    class Meta:
+        verbose_name = "AntiReq"
+        verbose_name_plural = "AntiReqs"
